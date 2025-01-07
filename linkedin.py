67d861b3f5839d2fbedaebb7cfd507bda0b5c8f5
SwapnilSonker <swapnilsonker1432@gmail.com>
2025-01-07 19:26:00 +0530

    search input done✅

--- linkedin.py
+++ linkedin.py
@@ -19,9 +19,28 @@ def login_to_linkedin(username, password):
         page.wait_for_url("https://www.linkedin.com/feed/")
         
         print("Logged in successfully!!")
+        
+        search_input = page.locator('.search-global-typeahead__input')
+
+# Wait for the search input field to be visible and interactable
+        search_input.wait_for(state='visible', timeout=10000)
+
+        # Click on the search input field
+        search_input.click()
+
+        print("Search field clicked successfully")
+        # search = page.locator('input[aria_label="Search"]')
+        # search.wait_for(state="visible", timeout=15000)
+        search_input.fill("YC 23")
+        search_input.press('Enter')
+        print("search enteres successfully")
+        # page.wait_for_timeout(10000)
         return page
+
+
+
+            
         
-        
-# if __name__ == "__main__":
-#     login_to_linkedin("swapnilsonker04@gmail.com", "Swapnil@04")        
+if __name__ == "__main__":
+    login_to_linkedin("swapnilsonker04@gmail.com", "Swapnil@04")        
         
