805055153fa17377265cabcfe8cb6d11013ef6b6
SwapnilSonker <swapnilsonker1432@gmail.com>
2025-01-09 18:28:12 +0530

    automation on scheduler done - docker pending

--- linkedin.py
+++ linkedin.py
@@ -1,6 +1,8 @@
-import re
 from playwright.sync_api import sync_playwright
 from time import sleep
+from apscheduler.schedulers.background import BackgroundScheduler 
+from apscheduler.events import EVENT_JOB_ERROR, EVENT_JOB_EXECUTED
+import threading
 
 def login_to_linkedin(username, password):
     with sync_playwright() as p:
@@ -25,16 +27,17 @@ def login_to_linkedin(username, password):
         search_input = page.locator('.search-global-typeahead__input')
         search_input.wait_for(state='visible', timeout=10000)
         search_input.click()
-        print("Search field clicked successfully")
+        # print("Search field clicked successfully")
         search_input.fill("YC")
         search_input.press('Enter')
         page.wait_for_timeout(10000)
         
         # Filter search results by "People"
         page.get_by_role("button", name="People").click()
-        print("Filtered by 'People'")
+        # print("Filtered by 'People'")
         request_connect = 0
-        while request_connect <= 10:
+        max_requests = 1
+        while request_connect <= max_requests:
             
             
             scroll_attempts = 0
@@ -51,14 +54,14 @@ def login_to_linkedin(username, password):
                     scroll_attempts += 1
                     continue
                 
-                scroll_attempts = 0  # Reset scroll attempts if buttons are found
+                # scroll_attempts = 0  # Reset scroll attempts if buttons are found
                 print(f"Found {count_connect} 'Connect' buttons on the page.")
                 
-                for i in range(count_connect):
+                for i in range(min(count_connect ,max_requests - request_connect)):
                     try:
                         # Click on the "Connect" button
                         connect_buttons.nth(i).click()
-                        print(f"Clicked 'Connect' button {i + 1}")
+                        # print(f"Clicked 'Connect' button {i + 1}")
                         page.wait_for_timeout(5000)
                         
                         # Check for the "Send" button and click if visible
@@ -66,14 +69,14 @@ def login_to_linkedin(username, password):
                         if send_button.is_visible():
                             send_button.click()
                             request_connect += 1
-                            print("Clicked 'Send' button.")
+                            # print("Clicked 'Send' button.")
                         else:
                             print("No 'Send' button found.")
                     except Exception as e:
                         print(f"Error clicking button {i + 1}: {e}")
                 
                 # Scroll down to load more buttons
-                print("Scrolling down to load more connections...")
+                # print("Scrolling down to load more connections...")
                 page.mouse.wheel(0, 1000)
                 sleep(5)
             next_button = page.locator('//button//span[text()="Next"]')   
@@ -84,8 +87,33 @@ def login_to_linkedin(username, password):
                 print("No next button is found.")
                 break
             
-        browser.close()         
-        
+        browser.close()        
+
+def job_listener(event):
+    if event.exception:
+        print(f"job failed: {event.job_id}")
+    else:
+        print(f"job completed successfully: {event.job_id}")
+    
+    print("scheduler exited from function")    
+    threading.Thread(target=scheduler.shutdown).start()
+            
+                
 
 if __name__ == "__main__":
-    login_to_linkedin("swapnilsonker04@gmail.com", "Swapnil@04")
+    # login = login_to_linkedin("swapnilsonker04@gmail.com", "Swapnil@04")
+    scheduler = BackgroundScheduler()
+    
+    scheduler.add_job(lambda:login_to_linkedin("swapnilsonker04@gmail.com", "Swapnil@04") , 'cron', hour=18, minute=16)
+    
+    scheduler.add_listener(job_listener, EVENT_JOB_EXECUTED | EVENT_JOB_ERROR)
+    try:
+        print("Scheduler started")
+        scheduler.start()
+        
+        while True:
+          sleep(2)
+    except (KeyboardInterrupt, SystemExit):
+        print("scheduler exited from main")
+        scheduler.shutdown()
+    
